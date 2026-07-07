84ac2c52a0bef2d2fe0e883a6aa229ee37bce990
jeannieteo <jeannieteo78@gmail.com>
2026-07-07 13:51:50 +0800

    soft assert did not assert all.

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -3,6 +3,9 @@ package tests;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import org.testng.annotations.Test;
 import base.BaseTest;
 import pageEvents.HomePageEvents;
@@ -24,15 +27,21 @@ public class HomePageTests extends BaseTest{
 		ArrayList<String> optionsText = csvReaderOptions.getOptions("src/main/resources/publicationOptions.csv"); //get the categories from the testdata file
 		int i =0;
 		for(WebElement option:options) {
-			softAssert.assertEquals(option.getText(), optionsText.get(i), "Option text does not match expected value: " + option.getText());
+			if(i != 0)	{
+				softAssert.assertEquals(option.getText(), optionsText.get(i), "Option text does not match expected value: " + option.getText());}
 			i++;
 			}
+		softAssert.assertAll(); // Assert all at the end to report all failures
 	}
 
 	@Test
 	public void selectAdType() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    	wait.until(ExpectedConditions.elementToBeClickable(HomePageElements.radioRunOn));
+    
 		driver.findElement(HomePageElements.radioRunOn).click();
 		softAssert.assertTrue(homepageEvents.isAdTypeSelected(HomePageElements.radioRunOn), "Run On ad type is not selected");
+		softAssert.assertAll(); // Assert all at the end to report all failures
 	}
 
 	@Test
@@ -42,23 +51,9 @@ public class HomePageTests extends BaseTest{
 		
 		int i = 0;
 		for(WebElement category : categories) {
-			System.out.println(category.getText().trim());
+			//System.out.println(category.getText().trim());
 			if(i != 0)	{
 ;			softAssert.assertEquals(category.getText().trim(), categoriesText.get(i), "Category text does not match expected value: " + category.getText());}
-			i++;
-		}
-		softAssert.assertAll(); // Assert all at the end to report all failures
-	}
-
-	@Test
-	public void verifyCategoryOptions1() {
-		List<WebElement> categories = homepageEvents.getCategoryOptions();
-		ArrayList<String> categoriesText = csvReaderOptions.getOptions("src/main/resources/categoryOptions.csv");
-		
-		int i = 0;
-		for(WebElement category : categories) {
-			System.out.println(category.getText().trim());
-			softAssert.assertEquals(category.getText().trim(), categoriesText.get(i), "Category text does not match expected value: " + category.getText());
 			i++;
 		}
 		softAssert.assertAll(); // Assert all at the end to report all failures
